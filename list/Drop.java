6d6461aa75e230bfe3a366d994bd8c51beb39b06
Brett Boston <bboston7@gmail.com>
2014-05-22 22:23:08 -0700

    Made diff use constructor

--- list/Drop.java
+++ list/Drop.java
@@ -1,6 +1,6 @@
 public class Drop<T> extends OopList<T> {
     public Drop(int at, OopList<T> list) {
         if (at == 0) this.list = new Empty<T>();
-        else list = this.list = new Insert<T>(list.head(), list.tail().drop(at - 1));
+        else list = this.list = new Insert<T>(list.head(), new Drop<T>(at - 1, list.tail()));
     }
 }
